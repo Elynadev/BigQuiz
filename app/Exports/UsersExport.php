5ac3a5fe991f8a7723525909e3562a3516da5c38
clement6292 <akouegnondjoi@gmail.com>
2024-12-09 16:10:22 +0100

    exportation des utilisateurs au top

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,15 @@ class UsersExport implements FromCollection
     public function collection()
     {
         return User::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Nom',
+            'Email',
+            'Rôle',
+        ];
     }
 }
